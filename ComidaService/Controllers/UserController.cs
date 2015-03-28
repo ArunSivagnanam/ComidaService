2c75c205da4c2d890211d83af6f3aa5a9e5d9d19
ArunSivagnanam <Arun.s@live.dk>
2015-03-28 14:00:13 +0100

    firstComit

---
+++ ComidaService/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using ComidaService.DataAccess;
+using ComidaService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ComidaService.Controllers
+{
+    public class UserController : ApiController
+    {
+        UserAccessor userAccessor;
+
+        public UserController()
+        {
+            userAccessor = new UserAccessor();
+        }
+
+        // GET api/user
+        public List<user> Get()
+        {
+            String email = "Arun.s@live.dk";
+            String password = "1234";
+
+            return userAccessor.validateUser(email, password);
+        }
+
+    }
+}
---
+++ ComidaService/DataAccess/UserAccessor.cs
@@ -0,0 +1,35 @@
+﻿using ComidaService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComidaService.DataAccess
+{
+    public class UserAccessor
+    {
+        public List<user> getAllUsers()
+        {
+            List<user> result = null;
+
+            using (var context = new Entities())
+            {
+                result = context.user.ToList();
+            }
+            return result;
+        }
+
+        public List<user> validateUser(String email, String password)
+        {
+            List<user> result = null;
+
+            using (var context = new Entities())
+            {
+                String quary = "SELECT * FROM `comida-db`.user where Email = @p0 and Password = @p1;";
+                result = context.user.SqlQuery(quary,email,password).ToList(); 
+            }
+            return result;
+        }
+
+    }
+}
